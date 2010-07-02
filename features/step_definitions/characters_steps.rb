9c28e55da8c7d2d092ae0e2828546d6eec32db45
James Holder <github.com@thirdtruck.org>
2010-07-02 15:19:06 -0400

    Add placeholder step definitions for character features.

---
+++ features/step_definitions/characters_steps.rb
@@ -0,0 +1,23 @@
+Given /^an empty script$/ do
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I add a character named "([^"]*)"$/ do |character_name|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^calling `([^`]*)` should return that character$/ do |command|
+  pending # express the regexp above with the code you wish you had
+end
+
+Given /^a character named "([^"]*)"$/ do |character_name|
+  pending # express the regexp above with the code you wish you had
+end
+
+When /^I call `([^`]*)`$/ do |command, string|
+  pending # express the regexp above with the code you wish you had
+end
+
+Then /^I should see "([^"]*)"$/ do |output|
+  pending # express the regexp above with the code you wish you had
+end
