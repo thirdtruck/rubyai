227279c838085627fda2c3049f8d64e142848475
James Holder <github.com@thirdtruck.org>
2010-07-05 16:34:12 -0400

    Apply the outstanding expansions to features/step_defintions/character_steps.rb, missed previously due to its renaming.

--- features/step_definitions/character_steps.rb
+++ features/step_definitions/character_steps.rb
@@ -1,25 +1,43 @@
 Given /^an empty script$/ do
-  pending # express the regexp above with the code you wish you had
+  @output = output
+  @game = RubyAi::Game.new(@output)
+  @results = []
 end
 
 When /^I add a character named "([^"]*)"$/ do |character_name|
-  pending # express the regexp above with the code you wish you had
-end
-
-Then /^calling `([^`]*)` should return that character$/ do |command|
-  pending # express the regexp above with the code you wish you had
-end
-
-Given /^a character named "([^"]*)"$/ do |character_name|
-  for_characters do
-    add :lucy, "Lucy"
+  @game.parse_script do
+    for_characters do
+      add character_name.downcase.to_sym, character_name
+    end
   end
 end
 
-When /^I call `([^`]*)`$/ do |command, string|
+Then /^the list of characters should include "([^`]*)"$/ do |character_name|
+  @game.characters.include?(character_name.downcase.to_sym)
+end
+
+Given /^a character named "([^"]*)"$/ do |character_name|
+  @game.parse_script do
+    for_characters do
+      add character_name.downcase.to_sym, character_name
+    end
+  end
+end
+
+When /^I call `([^`]*)`$/ do |command|
+  @output.puts @game.parse_script do
+    eval(command)
+  end
+end
+
+Then /^I should see "([^"]*)"$/ do |results|
+  @output.messages.include?(results)
+end
+
+When /^I give them an alias of "([^"]*)"$/ do |arg1|
   pending # express the regexp above with the code you wish you had
 end
 
-Then /^I should see "([^"]*)"$/ do |output|
+Then /^the character "([^"]*)" should be the same as "([^"]*)"$/ do |arg1, arg2|
   pending # express the regexp above with the code you wish you had
 end
