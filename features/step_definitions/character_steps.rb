881860dc99994a94e563461c7585e7784e02d3b6
James Holder <github.com@thirdtruck.org>
2010-07-07 21:29:20 -0400

    Explicitly declare the  feature as pending until needed.  Halt development of it for now.

--- features/step_definitions/character_steps.rb
+++ features/step_definitions/character_steps.rb
@@ -18,8 +18,15 @@ Given /^an? :(.*) character named "([^"]*)"$/ do |character_id, character_name|
   end
 end
 
-When /^I give them a pseudonym of :(.*) with name "([^"]*)"$/ do |pseudonym_id, pseudonym_name|
-  pending # express the regexp above with the code you wish you had
+When /^I give them a pseudonym of :(.*) named "([^"]*)"$/ do |pseudonym_id, pseudonym_name|
+  pending # Feature not yet needed
+  game.parse_script do
+    for_characters do
+      add character_id.to_sym, character_name do
+        pseudonym pseudonym_id.to_sym, pseudonym_name
+      end
+    end
+  end
 end
 
 Then /^the character :(.*) should be the same as :(.*)$/ do |first_character_id, second_character_id|
